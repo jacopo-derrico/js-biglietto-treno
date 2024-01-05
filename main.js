70d499f217f013413651446128f8a9a09076f2b7
jacopo-derrico <derricoj.design@gmail.com>
2024-01-05 12:32:13 +0100

    corrected and added €

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const kmPrice = 0.21
+const kmPrice = 0.21;
 
 
 // let tripLength = prompt("kilometri");
@@ -7,17 +7,17 @@ const kmPrice = 0.21
 let tripLength = document.getElementById("kilometers");
 let clientAge = document.getElementById("age");
 
-console.log(`Trip length: ${tripLength.value} Client age: ${clientAge.value}`)
-console.log(clientAge.value)
+// console.log(`Trip length: ${tripLength.value} Client age: ${clientAge.value}`)
+// console.log(clientAge.value)
 
 function calcTicket() {
     if (clientAge.value < 18) {
-        document.getElementById("result").innerHTML = (((kmPrice * tripLength.value) * 80) / 100).toFixed(2);
+        document.getElementById("result").innerHTML = ((((kmPrice * tripLength.value) * 80) / 100).toFixed(2) + '€');
     }
     else if (clientAge.value > 65) {
-        document.getElementById("result").innerHTML = (((kmPrice * tripLength.value) * 60) / 100).toFixed(2);
+        document.getElementById("result").innerHTML = ((((kmPrice * tripLength.value) * 60) / 100).toFixed(2) + '€');
     }
     else {
-        document.getElementById("result").innerHTML = kmPrice * tripLength.value
+        document.getElementById("result").innerHTML = ((kmPrice * tripLength.value).toFixed(2) + '€');
     }
 }
